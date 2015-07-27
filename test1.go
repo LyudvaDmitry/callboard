59b15132145d991d81c5b534c95362b0a0c1e4fb
LyudvaDmitry <lyudva.dmitry@gmail.com>
2015-07-27 21:17:41 +0300

    Fixed error handling

--- test1.go
+++ test1.go
@@ -4,6 +4,7 @@ import (
     "fmt"
     "net/http"
     "time"
+    "log"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -12,5 +13,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
     http.HandleFunc("/time", handler)
-    http.ListenAndServe(":8080", nil)
+    err := http.ListenAndServe(":8080", nil)
+    if err != nil {
+        log.Fatal(err)
+    }
 }
