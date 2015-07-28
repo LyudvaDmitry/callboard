447ab8b8ddb4517ceae4e967d574ce0357b59b30
LyudvaDmitry <lyudva.dmitry@gmail.com>
2015-07-28 14:46:47 +0300

    Made errs local; moved handler to HandleFunc() call

--- test1.go
+++ test1.go
@@ -7,17 +7,13 @@ import (
     "log"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    _, err := fmt.Fprint(w, time.Now().UTC())
-    if err != nil {
-        log.Fatal(err)
-    }
-}
-
 func main() {
-    http.HandleFunc("/time", handler)
-    err := http.ListenAndServe(":8080", nil)
-    if err != nil {
+    http.HandleFunc("/time", func (w http.ResponseWriter, r *http.Request) {
+	    if _, err := fmt.Fprint(w, time.Now().UTC()); err != nil {
+	        log.Fatal(err)
+	    }
+	} )
+    if err := http.ListenAndServe(":8080", nil); err != nil {
         log.Fatal(err)
     }
 }
