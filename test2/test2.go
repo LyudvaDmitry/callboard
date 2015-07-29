5cf6e5103b3cb939da7075b236dfd5676ed146c2
LyudvaDmitry <lyudva.dmitry@gmail.com>
2015-07-29 23:33:58 +0300

    Reworked to one template and one handler

--- test2/test2.go
+++ test2/test2.go
@@ -12,45 +12,32 @@ type Advert struct {
 //	Body  []byte
 }
 
-//html или javascript в объявлении?
-//Добавить многопоточность
-
-var templates = template.Must(template.ParseFiles("add.html", "adv.html"))
+var templates = template.Must(template.ParseFiles("view.html"))
 var adv_list []Advert
 
 func ViewHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Starting template execution")
-	for _, adv := range adv_list {
-		if err := templates.ExecuteTemplate(w, "adv.html", adv); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+	if r.FormValue("button") == "Save" {
+		log.Printf("Starting adding advert '%v'", r.FormValue("title"))
+		adv_list = append(adv_list, Advert{r.FormValue("title"), r.FormValue("body")})
+		log.Printf("Added advert '%v'", r.FormValue("title"))
+	} else if r.FormValue("button") == "Delete all adverts" {
+		log.Println("Starting deleting all adverts")
+		adv_list = make([]Advert, 0)
+		log.Println("Deleted all adverts")
 	}
-	if err := templates.ExecuteTemplate(w, "add.html", nil); err != nil {
+	log.Println("Starting template execution")
+	if err := templates.ExecuteTemplate(w, "view.html", adv_list); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	log.Println("Successfully executed templates")
 }
 
-func AddHandler(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Starting adding advert '%v'", r.FormValue("title"))
-	adv_list = append(adv_list, Advert{r.FormValue("title"), r.FormValue("body")})
-	http.Redirect(w, r, "/view", http.StatusFound)
-	log.Printf("Added advert '%v'", r.FormValue("title"))
-}
-
-func DelHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Starting deleting all adverts")
-	adv_list = make([]Advert, 0)
-	http.Redirect(w, r, "/view", http.StatusFound)
-	log.Println("Deleted all adverts")
-}
-
 func main() {
+	adv_list = make([]Advert, 2)
+	adv_list[0] = Advert{"Hello", "Look at me"}
+	adv_list[1] = Advert{"Go away", "Fucker"}
 	http.HandleFunc("/view", ViewHandler)
-	http.HandleFunc("/add", AddHandler)
-	http.HandleFunc("/deleteall", DelHandler)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
