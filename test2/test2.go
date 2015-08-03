5abc49566d0e7279ec5c9c1d256524851a7c492d
LyudvaDmitry <lyudva.dmitry@gmail.com>
2015-08-03 18:39:27 +0300

    Reworked 'accept' reading; Added time to adverts

--- test2/test2.go
+++ test2/test2.go
@@ -7,12 +7,17 @@ import (
 	"log"
 	"net/http"
 	"sync"
+    "time"
+	"strings"
+	"mime"
+	"strconv"
 )
 
 type Advert struct {
 	Title    string
 	Body     string
 	Username string
+	Time 	 string
 }
 
 var (
@@ -22,6 +27,48 @@ var (
 	port      = flag.String("port", ":8080", "Number of port to use. Example: ':8080'")
 )
 
+//Я уже сожалею, что я это написал.
+//Буду рад узнать, какая стандартная функция это делает.
+func IfMIMETypePreferred(r *http.Request, mtype string) (bool, error) {
+	log.Println(r.Header["Accept"])
+	accept := make([]string, 0)
+	for _, val := range r.Header["Accept"] {
+		accept = append(accept, strings.Split(val, ",")...)
+	}
+	mediatypes := make(map[string]map[string]string)
+	for _, val := range accept {
+		name, parameters, err := mime.ParseMediaType(val)
+		if err != nil {
+			return false, err
+		}
+		if _, present := parameters["q"]; !present {
+			parameters["q"] = "1"
+		}
+		mediatypes[name] = parameters
+	}
+	if mediatypes[mtype]["q"] == "" {
+		return false, nil
+	}
+	max := 0.0
+	for _, val := range mediatypes {
+		num, err := strconv.ParseFloat(val["q"], 32)
+		if err != nil {
+			return false, err
+		}
+		if num > max {
+			max = num
+		}
+	}
+	num, err := strconv.ParseFloat(mediatypes[mtype]["q"], 32)
+	if err != nil {
+		return false, err
+	}
+	if num == max {
+		return true, nil
+	}
+	return false, nil
+}
+
 func ViewHandler(w http.ResponseWriter, r *http.Request) {
 	username, _, ok := r.BasicAuth()
 	if !ok {
@@ -29,15 +76,22 @@ func ViewHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "401 Unauthorized", http.StatusUnauthorized)
 		return
 	}
-	if r.Header.Get("Accept") == "application/json" {
+	JSON, err := IfMIMETypePreferred(r, "application/json")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if JSON {
 		log.Printf("Sending JSON to %s", username)
 		b, err := json.Marshal(adv_list)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 		w.Header().Set("Content-Type", "application/json")
 		if _, err := w.Write(b); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 		return
 	}
@@ -47,13 +101,13 @@ func ViewHandler(w http.ResponseWriter, r *http.Request) {
 		switch button {
 		case "Save":
 			log.Printf("Adding advert '%v' as %s", title, username)
-			adv_list = append(adv_list, Advert{title, r.FormValue("body"), username})
+			adv_list = append(adv_list, Advert{title, r.FormValue("body"), username, time.Now().Format(time.RFC850)})
 		case "DeleteAll":
 			log.Printf("Deleting all adverts as %s", username)
 			adv_list = make([]Advert, 0)
 		}
 		mutex.Unlock()
-		http.Redirect(w, r, "/view", http.StatusFound)
+		http.Redirect(w, r, r.URL.String(), http.StatusFound)
 		return
 	}
 	log.Println("Executing templates")
