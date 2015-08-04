88f74ad0ad483a11306976382688bede8620bff8
LyudvaDmitry <lyudva.dmitry@gmail.com>
2015-08-04 18:56:17 +0300

    Added testcallboard.go

---
+++ testcallboard/testcallboard.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+	"github.com/LyudvaDmitry/test_repository/callboard"
+	"log"
+)
+
+func main() {
+	http.Handle("/board1/", callboard.NewCallboard())
+	cb := callboard.NewCallboard()
+	http.Handle("/board2/", cb)
+	*cb.Adverts = append(*cb.Adverts, callboard.Advert{"MyTitle", "Greetings from testcallboard.go", "User", "Time"})
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
